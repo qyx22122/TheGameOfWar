e0a97b36667e3bbda1ef1219949b3956db404497
nobody1902 <nobody1902@proton.me>
2024-09-20 20:09:28 +0200

    Updated main.c; Singleplayer now works.

--- main.c
+++ main.c
@@ -13,12 +13,29 @@ int main(){
 	Board b;
 	initBoard(&b);
 
+	bool turn = false;
+	Move move;
+
 	initWindow("The Game Of War", 800, 800);
 	
 	while(!windowShouldClose()) {
-		bool update = drawBoard(&b);
+		
+		int winCheck = checkWinCondition(&b);
 
-		if(update)
-			updateBoard(&b);
+		if(winCheck != 2) {
+			bool reset = drawEndScreen(winCheck == -1 ? 2 : winCheck, winCheck, &b);
+			if(reset) {
+				initBoard(&b);
+				turn = false;
+			}
+			continue;
+		}
+
+		move = drawBoard(&b, true, (PlayerColor)turn);
+		
+		if(move.type != NONE && validMove(&b, &move, turn)) {
+			updateBoardMove(&b, &move, turn);
+			turn = !turn;
+		}
 	}
 }
