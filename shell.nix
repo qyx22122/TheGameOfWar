5b78a73abac46e9282ced6d8551a226832f87940
qyx22122 <tibor.macek1@gmail.com>
2024-11-23 17:18:02 +0000

    New and improved shell.nix - it actually works nowž

--- shell.nix
+++ shell.nix
@@ -2,6 +2,24 @@
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
       gnumake
-      raylib 
+      gcc
+      glibc.dev
+      libxkbcommon.dev
+      xorg.libpthreadstubs
+      xorg.libX11.dev
+      xorg.libxcb.dev
+      xorg.libXext.dev
+      xorg.libXi.dev
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXinerama
+      xorg.xcbproto
+      xorg.xcbutil.dev
+      xorg.xcbutilcursor.dev
+      xorg.xcbutilerrors
+      xorg.xcbutilkeysyms.dev
+      xorg.xcbutilrenderutil.dev
+      xorg.xcbutilwm.dev
+      xorg.xorgproto
     ];
 }
