440931a19d4f99bc84a7e05216b1f9d4eac2b65c
qyx22122 <tibor.macek1@gmail.com>
2024-11-23 17:18:34 +0000

    Revert "New and improved shell.nix - it actually works now"
    This reverts commit 5b78a73abac46e9282ced6d8551a226832f87940.

--- shell.nix
+++ shell.nix
@@ -2,24 +2,6 @@
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
       gnumake
-      gcc
-      glibc.dev
-      libxkbcommon.dev
-      xorg.libpthreadstubs
-      xorg.libX11.dev
-      xorg.libxcb.dev
-      xorg.libXext.dev
-      xorg.libXi.dev
-      xorg.libXcursor
-      xorg.libXrandr
-      xorg.libXinerama
-      xorg.xcbproto
-      xorg.xcbutil.dev
-      xorg.xcbutilcursor.dev
-      xorg.xcbutilerrors
-      xorg.xcbutilkeysyms.dev
-      xorg.xcbutilrenderutil.dev
-      xorg.xcbutilwm.dev
-      xorg.xorgproto
+      raylib 
     ];
 }
